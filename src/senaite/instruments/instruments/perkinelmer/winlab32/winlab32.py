1213ca5208ffa4dd1d994bfd2b137b137679d0a2
Campbell <campbellmckellarbasset@gmail.com>
2021-10-11 22:19:47 +0200

    3369 still with winlab

--- src/senaite/instruments/instruments/perkinelmer/winlab32/winlab32.py
+++ src/senaite/instruments/instruments/perkinelmer/winlab32/winlab32.py
@@ -144,8 +144,7 @@ class Winlab32(InstrumentResultsFileParser):
         # maybe Reference Analysis
         ref_an = self.get_ref_an(sample_id)
         if ref_an:
-            parsed = {'result': value, 'DefaultResult': 'result'}
-            __import__('pdb').set_trace()
+            parsed = {'Result': value, 'DefaultResult': 'Result'}
             self._addRawResult(sample_id, parsed)
             return 0
 
